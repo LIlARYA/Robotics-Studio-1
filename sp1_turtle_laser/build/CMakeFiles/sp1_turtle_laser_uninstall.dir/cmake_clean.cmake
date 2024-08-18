61aed74c2aa9077fa1a7c015582e23d1af08bbd8
Arya <arya.nooralizadeh@gmail.com>
2024-08-18 15:27:50 +1000

    general commit

---
+++ sp1_turtle_laser/build/CMakeFiles/sp1_turtle_laser_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sp1_turtle_laser_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/sp1_turtle_laser_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
